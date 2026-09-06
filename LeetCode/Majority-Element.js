a4994fd6dd8d3ddf906877f8cd09f0235ef427a1
Siratim Mustakim Chowdhury <siratimmustakimchowdhury123@gmail.com>
2026-09-07 00:06:30 +0600

    feat: add solution for Majority Element

--- LeetCode/Majority-Element.js
+++ LeetCode/Majority-Element.js
@@ -1,21 +1,22 @@
-var majorityElement = function(nums) {
-    let counts = {}; 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+    let counts = {};
     let majorityCount = nums.length / 2;
-
     for (let num of nums) {
-
         if (num in counts) {
             counts[num]++;
         } else {
             counts[num] = 1;
         }
-
         if (counts[num] > majorityCount) {
-            return num; 
+            return num;
         }
     }
 
     return -1;
 };
 
-console.log(majorityElement([[1, 1, 2, 2]]));
+console.log(majorityElement([1, 1, 2, 2, 2, 1, 1]));
