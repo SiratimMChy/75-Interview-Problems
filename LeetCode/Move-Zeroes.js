132603810fed6e4aafbe9f8ec038980ca0c2df74
Siratim Mustakim Chowdhury <siratimmustakimchowdhury123@gmail.com>
2026-09-09 00:14:28 +0600

    feat: add solution for Move Zeroes.js

---
+++ LeetCode/Move-Zeroes.js
@@ -0,0 +1,18 @@
+var moveZeroes = function(nums) {
+    let j = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] !== 0){
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    for(let i = j; i < nums.length; i++ ){
+        nums[i] = 0;
+    }
+};
+let nums = [0,1,0,3,12];
+
+moveZeroes(nums);
+
+console.log(nums);
