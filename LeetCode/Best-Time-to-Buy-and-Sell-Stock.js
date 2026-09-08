34ce448ea37855aa444d2d14ef446d0de0b4852b
Siratim Mustakim Chowdhury <siratimmustakimchowdhury123@gmail.com>
2026-09-08 23:29:15 +0600

    feat: add solution for Best-Time-to-Buy-and-Sell-Stock

---
+++ LeetCode/Best-Time-to-Buy-and-Sell-Stock.js
@@ -0,0 +1,22 @@
+
+var maxProfit = function(prices) {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+
+    for (let i=0 ;i < prices.length; i++){
+
+        if( minPrice > prices[i] ){
+            minPrice = prices[i]
+        }
+
+        else {
+            let profit = prices[i]-minPrice;
+            if (profit> maxProfit){
+                maxProfit = profit;
+            }
+        }
+    }
+    return maxProfit;
+};
+
+console.log(maxProfit([7,1,5,3,6,4]));
